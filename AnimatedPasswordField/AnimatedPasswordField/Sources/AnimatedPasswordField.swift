0615e0ec8a8594a25c1b6f45b07e5e8f45914ed8
Nakul Chawla <nakul.chawla@quovantis.com>
2018-03-14 11:36:08 +0530

    Adding XCode Project

---
+++ AnimatedPasswordField/AnimatedPasswordField/Sources/AnimatedPasswordField.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatedPasswordField.swift
+//  AnimatedPasswordField
+//
+//  Created by Nakul Chawla on 14/03/18.
+//  Copyright © 2018 nakul. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedPasswordField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AnimatedPasswordField/AnimatedPasswordField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedPasswordField
+//
+//  Created by Nakul Chawla on 14/03/18.
+//  Copyright © 2018 nakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
